b76201df073f7221700d1ca7a68dce30b6d6a2c2
Ekta-14 <96908448+Ekta-14@users.noreply.github.com>
2023-11-25 00:34:41 +0530

    app icon set spannable string intent signup-signin

--- app/src/main/java/com/example/goldenhourambulance/sign_in.kt
+++ app/src/main/java/com/example/goldenhourambulance/sign_in.kt
@@ -1,11 +1,43 @@
 package com.example.goldenhourambulance
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class sign_in : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        //to set specific part of @dont_hav text to clickable
+        val textView: TextView = findViewById(R.id.dont_hav)
+        setClickableText(textView, R.string.dont_hav, "Sign up")
+    }
+
+    private fun setClickableText(textView: TextView, textResId: Int, clickableText: String) {
+        val fullText = getString(textResId)
+
+        val spannableString = SpannableString(fullText)
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                // Handle the click action here (e.g., navigate to the sign-in screen)
+                handleSignInClick()
+               // Toast.makeText(this@sign_in,"clicked",Toast.LENGTH_LONG).show()
+            }
+        }
+        spannableString.setSpan(clickableSpan,23,30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        textView.text=spannableString
+        textView.movementMethod= LinkMovementMethod.getInstance()
+    }
+    private fun handleSignInClick() {
+        val intent = Intent(this, signup::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/goldenhourambulance/signup.kt
+++ app/src/main/java/com/example/goldenhourambulance/signup.kt
@@ -1,11 +1,47 @@
 package com.example.goldenhourambulance
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.method.LinkMovementMethod.*
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 class signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        val textView: TextView = findViewById(R.id.already_hav)
+        setClickableText(textView, R.string.already_hav, "Sign in")
+    }
+
+    private fun setClickableText(textView: TextView, textResId: Int, clickableText: String) {
+        val fullText = getString(textResId)
+
+        val spannableString = SpannableString(fullText)
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                // Handle the click action here (e.g., navigate to the sign-in screen)
+                handleSignInClick()
+               //Toast.makeText(this@signup,"clicked",Toast.LENGTH_LONG).show()
+            }
+        }
+        spannableString.setSpan(clickableSpan,26,33, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        textView.text=spannableString
+        textView.movementMethod=LinkMovementMethod.getInstance()
+    }
+    private fun handleSignInClick() {
+        val intent = Intent(this, sign_in::class.java)
+        startActivity(intent)
     }
 }
